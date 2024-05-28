0d8f3a2c7c3ae1be968f5143a67106cc98d10d27
Benjamin Li <nicky01.bl@gmail.com>
2024-05-28 11:12:58 +0200

    test

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -1,8 +1,9 @@
-import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+
 class ApiService {
-  static const String baseUrl = 'http://192.168.1.21:8080';
+  static const String baseUrl = 'http://10.49.123.142:8080';
 
   Future<http.Response> signup(String email, String password) {
     return http.post(
