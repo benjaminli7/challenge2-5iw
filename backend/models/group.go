6a0177983dd08788e8def962798202e66309c4c7
syalicheff <yalicheff.sebastien@gmail.com>
2024-07-15 14:52:08 +0200

    Add Weather in group

--- backend/models/group.go
+++ backend/models/group.go
@@ -10,10 +10,10 @@ type Group struct {
 	UpdatedAt   time.Time `json:"updated_at"`
 	Description string    `json:"description"`
 	StartDate   time.Time `json:"start_date"`
-	Users 		[]*User `gorm:"many2many:group_users;"`
+	Users       []*User   `gorm:"many2many:group_users;"`
 	HikeID      uint      `json:"hike_id"`
 	Hike        Hike      `json:"hike" gorm:"foreignKey:HikeID"`
 	OrganizerID uint      `json:"organizer_id"`
 	Organizer   User      `json:"organizer" gorm:"foreignKey:OrganizerID"`
-	Messages []Message
+	Messages    []Message
 }
