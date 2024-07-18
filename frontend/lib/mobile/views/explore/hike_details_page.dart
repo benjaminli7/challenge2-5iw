1779cef1f21b39a44244e15de7a61c094784c8f9
Benjamin Li <nicky01.bl@gmail.com>
2024-07-18 07:55:54 +0200

    remove bg app icon

--- frontend/lib/mobile/views/explore/hike_details_page.dart
+++ frontend/lib/mobile/views/explore/hike_details_page.dart
@@ -9,12 +9,14 @@ import 'package:frontend/shared/services/group_service.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
+
 import '../../../shared/services/config_service.dart';
 
 class HikeDetailsExplorePage extends StatefulWidget {
   final Hike hike;
 
-  const HikeDetailsExplorePage({Key? key, required this.hike}) : super(key: key);
+  const HikeDetailsExplorePage({Key? key, required this.hike})
+      : super(key: key);
 
   @override
   _HikeDetailsExplorePageState createState() => _HikeDetailsExplorePageState();
@@ -31,7 +33,8 @@ class _HikeDetailsExplorePageState extends State<HikeDetailsExplorePage> {
     super.initState();
     final user = Provider.of<UserProvider>(context, listen: false).user;
     if (user != null) {
-      _groupsFuture = _groupService.fetchHikeGroups(user.token, widget.hike.id, user.id);
+      _groupsFuture =
+          _groupService.fetchHikeGroups(user.token, widget.hike.id, user.id);
     } else {
       _groupsFuture = Future.error(AppLocalizations.of(context)!.userNotLogged);
     }
@@ -58,17 +61,20 @@ class _HikeDetailsExplorePageState extends State<HikeDetailsExplorePage> {
         await _groupService.joinGroup(user.token, group.id, user.id);
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-            content: Text(AppLocalizations.of(context)!.joinGroupSuccess(group.id)),
+            content:
+                Text(AppLocalizations.of(context)!.joinGroupSuccess(group.id)),
             duration: const Duration(seconds: 2),
           ),
         );
         setState(() {
-          _groupsFuture = _groupService.fetchHikeGroups(user.token, widget.hike.id, user.id);
+          _groupsFuture = _groupService.fetchHikeGroups(
+              user.token, widget.hike.id, user.id);
         });
       } catch (e) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-            content: Text(AppLocalizations.of(context)!.joinGroupFailure(group.id, e.toString())),
+            content: Text(AppLocalizations.of(context)!
+                .joinGroupFailure(group.id, e.toString())),
             duration: const Duration(seconds: 2),
           ),
         );
@@ -122,7 +128,8 @@ class _HikeDetailsExplorePageState extends State<HikeDetailsExplorePage> {
                       Text(AppLocalizations.of(context)!.difficulty,
                           style: const TextStyle(fontWeight: FontWeight.bold)),
                       const SizedBox(height: 8),
-                      Text(widget.hike.difficulty, style: const TextStyle(fontSize: 16)),
+                      Text(widget.hike.difficulty,
+                          style: const TextStyle(fontSize: 16)),
                     ],
                   ),
                   Column(
@@ -130,7 +137,8 @@ class _HikeDetailsExplorePageState extends State<HikeDetailsExplorePage> {
                       Text(AppLocalizations.of(context)!.durationHour,
                           style: const TextStyle(fontWeight: FontWeight.bold)),
                       const SizedBox(height: 8),
-                      Text(widget.hike.duration.toString(), style: const TextStyle(fontSize: 16)),
+                      Text(widget.hike.duration.toString(),
+                          style: const TextStyle(fontSize: 16)),
                     ],
                   ),
                 ],
@@ -174,11 +182,14 @@ class _HikeDetailsExplorePageState extends State<HikeDetailsExplorePage> {
                   ),
                 ),
               const SizedBox(height: 16),
-              Text(
-                AppLocalizations.of(context)!.groups,
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  AppLocalizations.of(context)!.groups,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
               const SizedBox(height: 8),
@@ -204,7 +215,8 @@ class _HikeDetailsExplorePageState extends State<HikeDetailsExplorePage> {
                     List<Group> groups = snapshot.data!;
                     if (_selectedDate != null) {
                       groups = groups.where((group) {
-                        return group.startDate.isAtSameMomentAs(_selectedDate!) ||
+                        return group.startDate
+                                .isAtSameMomentAs(_selectedDate!) ||
                             group.startDate.isAfter(_selectedDate!);
                       }).toList();
                     }
@@ -283,20 +295,10 @@ class _HikeDetailsExplorePageState extends State<HikeDetailsExplorePage> {
               const Divider(),
               ListTile(
                 title: Text(AppLocalizations.of(context)!.viewReviews),
-                trailing: Row(
+                trailing: const Row(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    Icon(Icons.star, color: widget.hike.averageRating == 0 ? Colors.grey : Colors.yellow[700]),
-                    const SizedBox(width: 4),
-                    if (widget.hike.averageRating != 0)
-                      Text(
-                        widget.hike.averageRating.toStringAsFixed(1),
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    const Icon(Icons.arrow_forward_ios),
+                    Icon(Icons.arrow_forward_ios),
                   ],
                 ),
                 onTap: () {
